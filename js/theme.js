a02a0c8e786d3a35ef8f71a1467d8a2df21cabca
Mahmoud Ahmed <45967439+MAhmoud2527@users.noreply.github.com>
2019-05-06 18:26:23 +0200

    Add files via upload

--- js/theme.js
+++ js/theme.js
@@ -90,3 +90,33 @@ $('.stop').on('click',function(){
 
 
 })(jQuery)
+
+$(function(){
+    
+       
+    /* Arrow*/
+    var myButton = document.getElementById('goup');
+window.onscroll = function () {
+    
+    'use strict';
+    
+    if (window.pageYOffset >= 150)
+        {
+            
+            myButton.style.display = 'block';
+            
+        
+        } else {
+            
+            myButton.style.display = 'none';
+        }
+};
+
+$('.up').click(function(){
+    $('html, body').animate({scrollTop:0}, 'slow');
+}); 
+    
+    
+    
+    
+    });
